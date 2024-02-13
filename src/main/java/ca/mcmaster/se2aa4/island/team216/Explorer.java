0a914f4a195cb51a668a3e73f455ca756e021ce4
Vivian <vivnguyen1205@gmail.com>
2024-02-12 22:22:37 -0500

    temp code for adjustment

--- src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
@@ -9,13 +9,15 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class Explorer implements IExplorerRaid {
-
+public JSONObject info;
     private final Logger logger = LogManager.getLogger();
+
 
     @Override
     public void initialize(String s) {
+
         logger.info("** Initializing the Exploration Command Center");
-        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
+        info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
@@ -23,23 +25,35 @@ public class Explorer implements IExplorerRaid {
         logger.info("Battery level is {}", batteryLevel);
     }
 
+
+
     @Override
     public String takeDecision() {
+        String below = info.getString("found");
         JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
-        logger.info("** Decision: {}",decision.toString());
+        if(below.equals("GROUND")){
+            decision.put("action", "stop"); // we stop the exploration immediately
+        }
+        else{
+            decision.put("action", "fly"); // we stop the exploration immediately
+        }
+
+        logger.info("** Decision: {}", decision.toString());
         return decision.toString();
-    }
+}
+
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
+        info = extraInfo;
         logger.info("Additional information received: {}", extraInfo);
     }
 
