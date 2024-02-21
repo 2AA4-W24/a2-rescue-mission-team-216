a00259faf9cbb7a0c5589785a13de4005e0101a2
meena-alomran <meenaalomran@gmail.com>
2024-02-20 20:31:54 -0500

    flying E to end of map (close #7)

--- src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
@@ -9,15 +9,17 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class Explorer implements IExplorerRaid {
-public JSONObject info;
-    private final Logger logger = LogManager.getLogger();
 
+    private final Logger logger = LogManager.getLogger();
+    //private JSONObject response = new JSONObject();
+    private boolean i = true;
+    private Integer range = 0;
+    private String found = "";
 
     @Override
     public void initialize(String s) {
-
         logger.info("** Initializing the Exploration Command Center");
-        info = new JSONObject(new JSONTokener(new StringReader(s)));
+        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
@@ -25,36 +27,37 @@ public JSONObject info;
         logger.info("Battery level is {}", batteryLevel);
     }
 
-
-
     @Override
     public String takeDecision() {
-        String below = info.getString("found");
         JSONObject decision = new JSONObject();
-        if(below.equals("GROUND")){
-            decision.put("action", "stop"); // we stop the exploration immediately
+        JSONObject parameters = new JSONObject();
+        if (i) {
+            decision.put("parameters", parameters.put("direction", "E"));
+            decision.put("action", "echo");
+        } else {
+            if (range >= 1) decision.put("action", "fly");
+            else decision.put("action","stop");
         }
-        else{
-            decision.put("action", "fly"); // we stop the exploration immediately
-        }
-
-        logger.info("** Decision: {}", decision.toString());
+        logger.info("** Decision: {}",decision.toString());
         return decision.toString();
-}
-
+    }
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
-        info = extraInfo;
         logger.info("Additional information received: {}", extraInfo);
+
+        if (i) {
+            range = extraInfo.getInt("range");
+            found = extraInfo.getString("found");
+        }
+        i = !i;
     }
 
     @Override
