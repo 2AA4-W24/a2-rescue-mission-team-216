3413a87e1cbdff74e0774399c4cb44b81d565c20
Hareem <hareemsalman98@gmail.com>
2024-02-21 23:08:10 -0500

    refactoring takeDecision conditionals into a radar class

--- src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
@@ -15,56 +15,41 @@ public class Explorer implements IExplorerRaid {
     private boolean i = true;
     private Integer range = 0;
     private String found = "";
-    private int counter = 0;
     private int j = 5;
     private int k = 5;
+    private final Radar radar = new Radar(); //instance of radar takes care of watching the drone boundary
+
+
 
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Initialization info:\n {}",info.toString(2));
+        logger.info("** Initialization info:\n {}", info.toString(2));
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
+
+
     }
 
     @Override
     public String takeDecision() {
+
+        //takes in nothing as parameter
+        //uses AR's cost, status, and extra info?
         JSONObject decision = new JSONObject();
-        JSONObject parameters = new JSONObject();
-        /*if (i) {
-            decision.put("parameters", parameters.put("direction", "E"));
-            decision.put("action", "echo");
-        } else {
-            if (range >= 1) decision.put("action", "fly");
-            else decision.put("action","stop");
-        }*/
-        if (j > 0) {
-            decision.put("action", "fly");
-        } else if (j == 0) {
-            decision.put("parameters", parameters.put("direction", "S"));
-            decision.put("action", "heading");
-        }
-        else if (j == -1){
-            decision.put("parameters", parameters.put("direction", "W"));
-            decision.put("action", "heading");
-        } else {
-            if (k > 0) {
-                decision.put("action","fly");
-                k--;
-            }
-            else decision.put("action","stop");
-        }
-        logger.info("** Decision: {}",decision.toString());
-        return decision.toString();
+        String finalDecision = radar.getDecision(j, k, decision);
+        logger.info("** Decision: {}", finalDecision);
+        return finalDecision;
     }
 
     @Override
-    public void acknowledgeResults(String s) {
+    public void acknowledgeResults(String s) { //takes in the string which is the decision made
+        //returns nothing? interface specification won't allow return statements? I think?
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Response received:\n"+response.toString(2));
+        logger.info("** Response received:\n" + response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
@@ -73,11 +58,6 @@ public class Explorer implements IExplorerRaid {
         logger.info("Additional information received: {}", extraInfo);
 
         j--;
-
-        /*if (i) {
-            range = extraInfo.getInt("range");
-            found = extraInfo.getString("found");
-        i = !i;*/
     }
 
     @Override
--- src/main/java/ca/mcmaster/se2aa4/island/team216/Radar.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team216/Radar.java
@@ -1,5 +1,41 @@
 package ca.mcmaster.se2aa4.island.team216;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
 public class Radar {
+
+    private String Decision = "";
+    private final Logger logger = LogManager.getLogger();
+
+    public String getDecision(int j, int k, JSONObject decision) {
+        detection(j, k, decision);
+        return Decision;
+    }
+
+    private void detection(int j, int k, JSONObject decision){
+
+        JSONObject parameters = new JSONObject();
+
+        if (j > 0) {
+            decision.put("action", "fly"); //if within range, make the drone fly
+        } else if (j == 0) {
+            decision.put("parameters", parameters.put("direction", "S")); //change the direction
+            decision.put("action", "heading");
+        } else if (j == -1) {
+            decision.put("parameters", parameters.put("direction", "W")); //change the direction
+            decision.put("action", "heading");
+        } else {
+            if (k > 0) {
+                decision.put("action", "fly");
+                k--;
+            } else decision.put("action", "stop");
+        }
+
+        logger.info("** Decision: {}", decision.toString());
+        Decision = decision.toString();
+    }
+
 
 }
