3c92fc1bc7c0715fdaaca967648e220bb9e0f533
Hareem <hareemsalman98@gmail.com>
2024-03-01 18:15:07 -0500

    ignore

--- src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team216/Explorer.java
@@ -15,12 +15,12 @@ public class Explorer implements IExplorerRaid {
     private boolean i = true;
     private Integer range = 0;
     private String found = "";
+
     Boolean groundLocation = true;
     Boolean groundTravel = false;
-    Boolean criticalPoint = false;
+    Boolean creekLocate = false;
     Boolean siteLocate = false;
     Boolean goHome = false;
-    JSONObject extraInfo;
     private final Radar radar = new Radar(); //instance of radar takes care of watching the drone boundary
 
 
@@ -45,53 +45,44 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
-        Integer range=0;
-        JSONObject decision = new JSONObject();
-        String finalDecision = "";
 
         if (groundLocation){
-            if(extraInfo found == "GROUND"){
-                range = extraInfo range
-
-            }
-            else{
-                drone.compass turn left = newDirection;
-                decision.put("parameters", parameters.put("direction", newDirection));
-            }
-
             //drone.findGround (a method within drone)
-
-            //if current dir returns ground via radar awesome store range to be used in next phase
-            //boolean groundLocate to false
-            //set boolean groundTravel to true
-            //decision = do nothing I guess u just located the ground and how far it is
-            //if not then decision = change current dir
+                //if current dir returns ground via radar awesome store range to be used in next phase
+                    //boolean groundLocate to false
+                    //set boolean groundTravel to true
+                    //decision = do nothing I guess u just located the ground and how far it is
+                //if not then decision = change current dir
             //return the decision
         }
 
         else if (groundTravel) {
-            if (range != 0){
-                finalDecision = String.valueOf(decision.put("action", "fly"));
-                range--;
-            }
-            else{
-                groundTravel = false;
-                criticalPoint = true;
-            }
-            return finalDecision;
+
+            //int range = range recieived from previous phase (e.g. 42)
+
+            //if range != 0
+                //decision = action fly
+                //range --
+            //else
+                //boolean groundTravel = false
+                //boolean creekLocate = true
         }
-        else if (criticalPoint){
 
-            // grid search
-
+        else if (creekLocate){
             //if we are still near perimeter of island
-            //photoscanner checks for creeks
-            //while there are still creeks to be found
-            //if a creek is found, append to creek array
-            //decision = action fly
+                //photoscanner checks for creeks
+                //while there are still creeks to be found
+                    //if a creek is found, append to creek array
+                   //decision = action fly
             //decision = move to get closer to perimeter (direction headed will depend on conditons)
 
             //if all creeks are found bool creekLocate = false and siteLocate=true
+        }
+
+        else if(siteLocate){
+            //repeat same process as creek locate except you do not need to ensure you are in the perimeter of the island
+            //do island grid search
+            //once creek is found then siteLocate = false goHome = true
         }
 
         else{
@@ -102,7 +93,6 @@ public class Explorer implements IExplorerRaid {
         return "Sorry";
     }
 
-
     @Override
     public void acknowledgeResults(String s) { //takes in the string which is the decision made
         //returns nothing? interface specification won't allow return statements? I think?
@@ -112,7 +102,7 @@ public class Explorer implements IExplorerRaid {
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
-        extraInfo = response.getJSONObject("extras");
+        JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
 
     }
@@ -123,3 +113,6 @@ public class Explorer implements IExplorerRaid {
     }
 
 }
+
+
+
